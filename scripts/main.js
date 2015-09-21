b290aa942e67b5556a1712a507195104c64d1e32
Mike Mathew <m2mathew@me.com>
2015-09-21 18:46:29 -0500

    Styling and background for pages completed.

--- scripts/main.js
+++ scripts/main.js
@@ -33,9 +33,37 @@ $(document).ready(function(){
     $('nav').hide();
     $('#loading-screen').show();
 
+    $('#loading-box').animate(
+        { width: '100%', opacity: '0.5' },
+        4000, 'linear', function() {
+            console.log('Hello, brother.');
+        });
+
+    showLoadingDots();
+
     setTimeout(function() {
         $('#loading-screen').hide();
         $('nav').show();
     }, 4000);
 
 });
+
+var showLoadingDots = function() {
+    var showDots = setInterval(function(){
+        if ($('#loading-text').length>0) {
+            var dots = '...', i=1;
+        if ($('#loading-text').html().length === 0 || ($('#loading-text').html().length === dots.length)){
+            $('#loading-text').html('');
+            var i = 1;
+        }
+        else {
+            i++;
+        }
+        $('#loading-text').html($('#loading-text').html() + '.');
+        }
+        else {
+           clearInterval(showDots);
+        }
+    }, 400);
+}
+
